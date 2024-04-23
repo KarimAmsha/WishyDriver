e3655428df70151af531a120bde96fa4a7f7ed72
Karim Amsha <karim.amsha@gmail.com>
2024-04-23 22:07:16 +0300

    Initial Commit

---
+++ Wishy/WishyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishyApp.swift
+//  Wishy
+//
+//  Created by Karim Amsha on 23.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct WishyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
